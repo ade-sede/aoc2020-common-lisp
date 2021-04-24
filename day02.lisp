65105039951775d37327481271a6358661ee648f
Adrien DE SEDE <adrien.de.sede@gmail.com>
2021-04-24 21:01:35 +0200

    Day02 part1

---
+++ day02.lisp
@@ -0,0 +1,21 @@
+(ql:quickload :cl-ppcre)
+
+(defvar +rules-and-passwords+ (uiop:read-file-lines "02.input"))
+
+(defun parse-rule-and-password (s)
+  (cl-ppcre:register-groups-bind
+	  (min max key password)
+	  ("(\\d*)-(\\d*) (.): (.*)" s)
+	(list min max key password)))
+
+
+
+(print (count-if #'(lambda (rule-and-password)
+			  (let ((min (parse-integer (first rule-and-password)))
+					(max (parse-integer (second rule-and-password)))
+					(key (character (third rule-and-password)))
+					(password (fourth rule-and-password)))
+				(flet ((in-range (n)
+						 (and (<= n max) (<= min n))))
+				  (in-range (count-if #'(lambda (c) (char= c key)) password)))))
+			  (mapcar #'parse-rule-and-password +rules-and-passwords+)))
