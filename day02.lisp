7de76436cb5eb00459d552b943d9daa085523666
Adrien DE SEDE <adrien.de.sede@gmail.com>
2021-04-24 21:05:17 +0200

    Extracted the logic specific to part1 out of the lambda, into a function

--- day02.lisp
+++ day02.lisp
@@ -8,10 +8,7 @@
 	  ("(\\d*)-(\\d*) (.): (.*)" s)
 	(list min max key password)))
 
-
-
-(defun part1 ()
-(print (count-if #'(lambda (rule-and-password)
+(defun valid1 (rule-and-password)
 			  (let ((min (parse-integer (first rule-and-password)))
 					(max (parse-integer (second rule-and-password)))
 					(key (character (third rule-and-password)))
@@ -19,4 +16,6 @@
 				(flet ((in-range (n)
 						 (and (<= n max) (<= min n))))
 				  (in-range (count-if #'(lambda (c) (char= c key)) password)))))
-			  (mapcar #'parse-rule-and-password +rules-and-passwords+))))
+
+(defun part1 ()
+  (print (count-if #'valid1 (mapcar #'parse-rule-and-password +rules-and-passwords+))))
